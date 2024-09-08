ac224a4907025a473565e545c5f9644456c7af2e
muiz <muiz.dev@gmail.com>
2024-09-07 22:01:56 -0700

    Replace iostream w/ spdlog & fmt

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,6 @@
 #include <cstring>
 #include <exception>
 #include <filesystem>
-#include <iostream>
 #include <optional>
 #include <string>
 #include <unordered_map>
@@ -17,8 +16,34 @@
 #include "OgreRTShaderSystem.h"
 #include "OgreRoot.h"
 #include "toml++/toml.hpp"
+// clang-format off
+#include "fmt/core.h"
+#define SPDLOG_FMT_EXTERNAL
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+// clang-format on
 
 namespace {
+
+class streamable_logger_t {
+  spdlog::level::level_enum log_level_;
+  std::ostringstream log_stream_;
+
+public:
+  explicit streamable_logger_t(const spdlog::level::level_enum log_level)
+      : log_level_(log_level) {}
+
+  template <typename T> streamable_logger_t &operator<<(const T &value) {
+    log_stream_ << value;
+    return *this;
+  }
+
+  ~streamable_logger_t() { spdlog::log(log_level_, "{}", log_stream_.str()); }
+};
+
+// loguru
+#define LOG_S(log_level) streamable_logger_t(spdlog::level::log_level)
+#define LOG(log_level) LOG_S(log_level)
 
 class AssetLibrary {
 public:
@@ -33,13 +58,13 @@ public:
     try {
       tbl = toml::parse_file(std::string(config_path));
     } catch (const toml::parse_error &err) {
-      std::cerr << "Parsing failed: " << err << std::endl;
+      LOG(err) << "Parsing failed: " << err;
       return std::nullopt;
     }
 
     const toml::table *objects_table = tbl["assets"].as<toml::table>();
     if (!objects_table) {
-      std::cerr << "Could not find any assets in the config." << std::endl;
+      LOG(err) << "Could not find any assets in the config.";
       return std::nullopt;
     }
 
@@ -47,16 +72,16 @@ public:
     lib.asset_dir_ = config_path.parent_path();
     for (const auto &object : *objects_table) {
       const toml::key &object_name = object.first;
-      std::cout << "Loading object: " << object_name << std::endl;
+      SPDLOG_INFO(fmt::format("Loading object: {}", std::string(object_name)));
       const toml::table *objinfo = object.second.as<toml::table>();
       if (!objinfo) {
-        std::cerr << "Error: No object info found." << std::endl;
+        LOG(err) << "Error: No object info found.";
         continue;
       }
       if (!lib.ParseObjectInfoFromTOML(object_name, *objinfo)) {
-        std::cerr << "Error: Failed to parse asset object information for "
-                     "object name: "
-                  << object_name << std::endl;
+        LOG(err) << "Error: Failed to parse asset object information for "
+                    "object name: "
+                 << object_name;
         return std::nullopt;
       }
     }
@@ -80,7 +105,7 @@ private:
     std::optional<std::string_view> object_path =
         obj_info["path"].value<std::string_view>();
     if (!object_path.has_value()) {
-      std::cerr << "No path found for object: " << object_key << std::endl;
+      LOG(err) << "No path found for object: " << object_key;
       return false;
     }
 
@@ -89,13 +114,13 @@ private:
     info.resource_relative_path = object_path.value();
 
     if (!std::filesystem::exists(info.absolute_path)) {
-      std::cerr << "Error: Asset at path " << info.absolute_path
-                << " does not exist." << std::endl;
+      LOG(err) << "Error: Asset at path " << info.absolute_path
+               << " does not exist.";
       return false;
     }
     if (std::filesystem::is_directory(info.absolute_path)) {
-      std::cerr << "Error: Asset at path " << info.absolute_path
-                << "is a directory." << std::endl;
+      LOG(err) << "Error: Asset at path " << info.absolute_path
+               << "is a directory.";
       return false;
     }
     assets_.insert({std::string(object_key), std::move(info)});
@@ -190,21 +215,22 @@ void Scene::ParseEntityInfoFromTOML(const toml::table &entity_info,
   std::optional<std::string_view> mesh_name =
       entity_info["mesh"].value<std::string_view>();
   if (!mesh_name.has_value()) {
-    std::cerr << "Error: Could not get mesh name from entity info."
-              << std::endl;
+    LOG(err) << "Error: Could not get mesh name from entity info.";
     return;
   }
 
   const AssetLibrary::AssetInfo *asset_info = assetlib.GetAsset(*mesh_name);
   if (!asset_info) {
-    std::cerr << "Error: Could not find asset from asset library with name: "
-              << *mesh_name << std::endl;
+    LOG(err) << "Error: Could not find asset from asset library with name: "
+             << *mesh_name;
     return;
   }
   std::filesystem::path resource_relative_path_as_fspath(
       asset_info->resource_relative_path);
-  std::cout << "Resource relative path: " << asset_info->resource_relative_path
-            << ", filename: " << resource_relative_path_as_fspath.filename();
+  SPDLOG_INFO(
+      fmt::format("Resource relative path: {}, filename: {}",
+                  std::string(asset_info->resource_relative_path),
+                  std::string(resource_relative_path_as_fspath.filename())));
 
   Ogre::Entity *entity =
       scene.createEntity(resource_relative_path_as_fspath.filename());
@@ -233,26 +259,27 @@ void Scene::Load(OgreBites::ApplicationContext *appctx,
 
   light_node->setPosition(20, 80, 50);
 
-  std::cout << "Reading scene config from file: " << config_ << std::endl;
+  SPDLOG_INFO(
+      fmt::format("Reading scene config from file: {}", std::string(config_)));
   toml::table scene_tbl;
   try {
     scene_tbl = toml::parse_file(std::string(config_));
   } catch (const toml::parse_error &err) {
-    std::cerr << "Error: Failed to parse scene toml: " << err << std::endl;
+    LOG(err) << "Error: Failed to parse scene toml: " << err;
     return;
   }
 
   const toml::table *cameras_table = scene_tbl["cameras"].as<toml::table>();
   if (!cameras_table) {
-    std::cerr << "Could not find any cameras in the scene config." << std::endl;
+    LOG(err) << "Could not find any cameras in the scene config.";
     return;
   }
   for (const auto &camera : *cameras_table) {
     const toml::key &camera_name = camera.first;
-    std::cout << "Loading camera: " << camera_name << std::endl;
+    SPDLOG_INFO(fmt::format("Loading camera: {}", std::string(camera_name)));
     const toml::table *camerainfo = camera.second.as<toml::table>();
     if (!camerainfo) {
-      std::cerr << "Error: No camera info found." << std::endl;
+      LOG(err) << "Error: No camera info found.";
       continue;
     }
     ParseCameraInfoFromTOML(camera_name, *camerainfo, *scn_manager, *appctx);
@@ -260,22 +287,21 @@ void Scene::Load(OgreBites::ApplicationContext *appctx,
 
   const toml::table *entities_table = scene_tbl["entities"].as<toml::table>();
   if (!entities_table) {
-    std::cerr << "Could not find any entities in the scene config."
-              << std::endl;
+    LOG(err) << "Could not find any entities in the scene config.";
     return;
   }
 
   for (const auto &object : *entities_table) {
     const toml::key &object_name = object.first;
-    std::cout << "Loading entity: " << object_name << std::endl;
+    SPDLOG_INFO(fmt::format("Loading entity: {}", std::string(object_name)));
     const toml::table *objinfo = object.second.as<toml::table>();
     if (!objinfo) {
-      std::cerr << "Error: No object info found." << std::endl;
+      LOG(err) << "Error: No object info found.";
       continue;
     }
     ParseEntityInfoFromTOML(*objinfo, *scn_manager, assetlib);
   }
-  std::cout << "Finished loading entities for scene." << std::endl;
+  SPDLOG_INFO("Finished loading entities for scene.");
 }
 
 enum ConfigStrictness { UNKNOWN = 0, REQUIRED, OPTIONAL };
@@ -287,14 +313,13 @@ enum ConfigStrictness { UNKNOWN = 0, REQUIRED, OPTIONAL };
     switch (STRICTNESS) {                                                      \
     case ConfigStrictness::REQUIRED: {                                         \
       if (!value.has_value()) {                                                \
-        std::cerr << "Failed to parse toml settings: " << #TOML_FIELD          \
-                  << std::endl;                                                \
+        LOG(err) << "Failed to parse toml settings: " << #TOML_FIELD;          \
         return std::nullopt;                                                   \
       }                                                                        \
     } break;                                                                   \
     case ConfigStrictness::OPTIONAL: {                                         \
       if (!value.has_value()) {                                                \
-        std::cout << "Ignoring unset field: " << #TOML_FIELD << std::endl;     \
+        SPDLOG_INFO("Ignoring unset field: " #TOML_FIELD);                     \
         __should_set_value = false;                                            \
       }                                                                        \
     } break;                                                                   \
@@ -317,7 +342,7 @@ public:
     try {
       tbl = toml::parse_file(std::string(config_path));
     } catch (const toml::parse_error &err) {
-      std::cerr << "Parsing failed: " << err << std::endl;
+      LOG(err) << "Parsing failed: " << err;
       return std::nullopt;
     }
 
@@ -371,7 +396,7 @@ private:
 void Game::ParseScenes() {
   const std::filesystem::path scenes_path = project_path_ / "scenes";
   if (!std::filesystem::exists(scenes_path)) {
-    std::cerr << "Failed to find scenes directlry for project." << std::endl;
+    LOG(err) << "Failed to find scenes directlry for project.";
     return;
   }
 
@@ -389,22 +414,22 @@ void Game::ParseScenes() {
     const std::string &scene_name = path.filename();
     assert(!scene_name.empty());
     if (scenes_.find(scene_name) != scenes_.end()) {
-      std::cerr << "WARN: duplicate scene name: " << scene_name;
+      LOG(warn) << "duplicate scene name: " << scene_name;
       continue;
     }
     Scene scene(scene_config_path);
     scenes_.insert({scene_name, scene});
-    std::cout << "Added new scene: " << scene_name << std::endl;
+    SPDLOG_INFO(fmt::format("Adding new scene: {}", scene_name));
   }
 }
 
 void Game::LoadStartingScene() {
   const std::string starting_scene(cfg_.StartingScene().data(),
                                    cfg_.StartingScene().size());
-  std::cout << "Loading starting scene: " << starting_scene << std::endl;
+  SPDLOG_INFO(fmt::format("Loading starting scene: {}", starting_scene));
   auto starting_scene_itr = scenes_.find(starting_scene);
   if (starting_scene_itr == scenes_.end()) {
-    std::cerr << "Error: starting scene does not exist: " << starting_scene;
+    LOG(err) << "Error: starting scene does not exist: " << starting_scene;
     return;
   }
   starting_scene_itr->second.Load(this, assetlib_);
@@ -419,10 +444,10 @@ void Game::createRoot() {
 
 void Game::initApp() {
   createRoot();
-  std::cout << "Loading GL renderer." << std::endl;
+  SPDLOG_INFO("Loading GL renderer.");
   getRoot()->loadPlugin("/home/mob/Projects/roguelike/build/_deps/ogre-build/"
                         "lib/RenderSystem_GL");
-  std::cout << "Finished loading GL renderer." << std::endl;
+  SPDLOG_INFO("Finished loading GL renderer.");
   ApplicationContext::initApp();
 }
 
@@ -454,15 +479,14 @@ void RunGameProject(const std::filesystem::path project_path) {
   // Parse the project toml file.
   const std::filesystem::path project_toml_path = project_path / "project.toml";
   if (!std::filesystem::exists(project_toml_path)) {
-    std::cerr << "Error: project.toml not found at " << project_toml_path
-              << std::endl;
+    LOG(err) << "Error: project.toml not found at " << project_toml_path;
     return;
   }
 
   std::optional<GameConfig> game_config =
       GameConfig::FromTOML(project_toml_path);
   if (!game_config.has_value()) {
-    std::cerr << "Error: Failed to parse game config." << std::endl;
+    LOG(err) << "Error: Failed to parse game config.";
     return;
   }
 
@@ -471,7 +495,7 @@ void RunGameProject(const std::filesystem::path project_path) {
   std::optional<AssetLibrary> asset_lib =
       AssetLibrary::FromTOML(asset_library_path);
   if (!asset_lib.has_value()) {
-    std::cerr << "Error: Could not parse asset library config." << std::endl;
+    LOG(err) << "Error: Could not parse asset library config.";
     return;
   }
 
@@ -481,8 +505,7 @@ void RunGameProject(const std::filesystem::path project_path) {
     game.getRoot()->startRendering();
     game.closeApp();
   } catch (const std::exception &e) {
-    std::cerr << "Error occurred during game execution: " << e.what()
-              << std::endl;
+    LOG(err) << "Error occurred during game execution: " << e.what();
     return;
   }
 }
@@ -502,12 +525,11 @@ int main(int argc, char **argv) {
   const std::filesystem::path current_path = std::filesystem::current_path();
   const std::filesystem::path full_project_path = current_path / project_path;
   if (!std::filesystem::exists(full_project_path)) {
-    std::cerr << "Error: Invalid project path. Path does not exist."
-              << std::endl;
+    LOG(err) << "Error: Invalid project path. Path does not exist.";
     return 1;
   }
   if (!std::filesystem::is_directory(full_project_path)) {
-    std::cerr << "Error: Project path is not a directory." << std::endl;
+    LOG(err) << "Error: Project path is not a directory.";
     return 1;
   }
   RunGameProject(full_project_path);
